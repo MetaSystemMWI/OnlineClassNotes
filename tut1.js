7da2d2a05b0e9f745032f7be511b7cac991701cc
MetaSystemMWI <128566079+MetaSystemMWI@users.noreply.github.com>
2023-07-20 19:35:12 +0500

    Add files via upload

---
+++ tut1.js
@@ -0,0 +1,45 @@
+// alert("Welcome to JS");
+
+
+// let x; //statement-1
+
+// output method
+// innerHTML, document.write(), window.alert(), console.log()
+
+// console.log("Hello World!");
+
+
+// console.log(5 + 6);
+
+// HOW TO CREATE VARIABLES
+
+// var x;
+// x = "Ali";
+// let y;
+// y = 3;
+// let z;
+// z = 5.6;
+// let a;
+// a = 'A';
+// = asignment operator right side , left side: left side variable name and right side values
+//5*5 expression
+
+
+//comments single line comments 
+/* comments 
+multiple lines */
+
+
+// Camel case   pascal case
+// first_name,    First_Name
+//x , X small case 
+//let: //keywords (Pre-define words thats functions already mentioned)
+// connot be redeclared
+// must be declared before use 
+// have block scope
+
+let x = "Ali";
+alert(x); {
+    let x = 3;
+    alert(x);
+}
